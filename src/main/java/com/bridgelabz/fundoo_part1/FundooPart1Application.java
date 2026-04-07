a0e583734b65fa75e278c0d6e04f1f8c7343ebc8
Naveen <naveenudai26@gmail.com>
2026-04-07 12:05:57 +0530

    [Naveen]:[Add]: Initial commit

---
+++ src/main/java/com/bridgelabz/fundoo_part1/FundooPart1Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoo_part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/fundoo_part1/FundooPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoo_part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
